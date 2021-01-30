5c7e0d3a719342510d9471a8828ebb00b2c70eb0
zhorsgame <juanmiguelsaugarcanete@gmail.com>
2021-01-30 20:39:32 +0100

    Screenshot 6 Main6

--- src/proyectojava/Main5.java
+++ src/proyectojava/Main5.java
@@ -15,7 +15,20 @@ public class Main5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int [] numeros = {5,25,36,2,14,25,3,1};
+        int [] numeros2 = new int[numeros.length];
+        System.arraycopy(numeros , 0, numeros2, 0, numeros.length);
+        System.out.println("Array 1");
+        for(int i=0;i<numeros.length;i++){
+            
+            System.out.print("Array 1 :"+numeros[i]+",");
+        }
+        System.out.println(" ");
+        System.out.println("Array 2");
+        for(int z=0;z<numeros2.length;z++){
+            
+            System.out.print("Array 2 :"+numeros2[z]+",");
+        }
     }
     
 }
