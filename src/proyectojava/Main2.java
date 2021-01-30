7e89508ec7923aeb9d42f12d9ba06d79257ff885
zhorsgame <juanmiguelsaugarcanete@gmail.com>
2021-01-30 20:27:29 +0100

    Screenshot 2 Main2

---
+++ src/proyectojava/Main2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectojava;
+
+/**
+ *
+ * @author juanmi
+ */
+public class Main2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[] nombre = {"H","O","L","A"}; //Declarar un array tipo cadena
+        //Recorer el array
+        
+        for(int i=0; i<nombre.length; i++){
+            System.out.print(nombre[i]);
+            
+        }
+        System.out.println("");
+    }
+    
+}
